23cd669ef8afaf5ef296e49ea7bce34f13811b85
Sean Proctor <sproctor@gmail.com>
2018-03-16 12:59:58 -0400

    Basics to move to next page

--- src/main/kotlin/cc/warlock/warlock3/view/SgeWizard.kt
+++ src/main/kotlin/cc/warlock/warlock3/view/SgeWizard.kt
@@ -3,6 +3,7 @@ package cc.warlock.warlock3.view
 import cc.warlock.warlock3.controller.WarlockClientController
 import cc.warlock.warlock3.model.AccountModel
 import cc.warlock.warlock3.stormfront.*
+import javafx.scene.control.TableView
 import tornadofx.*
 
 class SgeWizard : Wizard("Connect character", "Provide account details to connect using SGE"){
@@ -13,7 +14,7 @@ class SgeWizard : Wizard("Connect character", "Provide account details to connec
     }
 }
 
-class AccountInput : View("Account") {
+class AccountInput(val wizard: Wizard) : View("Account") {
     val account : AccountModel by inject()
     val controller: WarlockClientController by inject()
 
@@ -45,19 +46,33 @@ class AccountInput : View("Account") {
     }
 
     override fun onSave() {
-        println("onsave")
         // TODO lookup account/save account here
         controller.connection.connect()
         isComplete = false
+        if (wizard.currentPage == this) {
+            wizard.next()
+        }
     }
 }
 
 class GameSelector : View("Game") {
     val controller: WarlockClientController by inject()
+    var table: TableView<SgeGame>? = null
+
+    inner class GameSgeListener : SgeConnectionListener {
+        override fun event(event: SgeEvent) {
+            when (event) {
+                is SgeGamesReadyEvent -> {
+                    table.items = games
+                }
+            }
+        }
+    }
+
     override val root = form {
         fieldset(title) {
             field("Game") {
-                tableview<SgeGame> {
+                table = tableview(controller.games) {
                     readonlyColumn("title", SgeGame::title)
                     bindSelected(controller.gameModel)
                 }
