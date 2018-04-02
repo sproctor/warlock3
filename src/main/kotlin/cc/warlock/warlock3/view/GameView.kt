839c41326cac3e4d074177e572ff85d8fe05b5ed
sproctor <sproctor@gmail.com>
2018-04-02 16:35:05 -0400

    Switch to WebView

--- src/main/kotlin/cc/warlock/warlock3/view/GameView.kt
+++ src/main/kotlin/cc/warlock/warlock3/view/GameView.kt
@@ -1,17 +1,19 @@
 package cc.warlock.warlock3.view
 
 import cc.warlock.warlock3.core.*
+import javafx.concurrent.Worker
 import javafx.scene.control.ScrollPane
 import javafx.scene.paint.Color
 import javafx.scene.text.Font
 import javafx.scene.text.Text
 import tornadofx.*
 import java.util.*
+import kotlin.concurrent.thread
 
 // GameView is a bit of a misnomer. It consists of the text view and the text entry
 class GameView(client: WarlockClient) : Fragment() {
     private val listeners = LinkedList<WarlockClient.ClientViewListener>()
-    private val output = textflow { }
+    private val output = webview { }
     private val input = textfield {
         setOnAction {
             listeners.forEach { it.commandEntered(text) }
@@ -20,11 +22,26 @@ class GameView(client: WarlockClient) : Fragment() {
     }
     override val root = borderpane {
         title = "Game View"
-        center = scrollpane {
-            add(output)
-            setFitToWidth(true)
-            prefWidthProperty().bind(this@borderpane.widthProperty())
-        }
+        center = output
+        output.prefWidthProperty().bind(this@borderpane.widthProperty())
+        output.engine.loadContent("""
+            <!doctype html>
+            <html>
+            <head>
+            <script>
+                window.load = function() {
+                    var observer = new MutationObserver(function(mutations, o) {
+                        if (mutations[0].addedNodes.length > 0)
+                            scrollToBottom();
+                    });
+                    observer.observe(document.body, {childList: true, subtree: true });
+                });
+                function scrollToBottom() {
+                    window.scrollTo(0, document.body.scrollHeight);
+                }
+            </script>
+            </head><body></body></html>
+            """)
         bottom = input
         input.prefWidthProperty().bind(this@borderpane.widthProperty())
     }
@@ -53,16 +70,28 @@ class GameView(client: WarlockClient) : Fragment() {
     }
 
     private fun displayText(text: Text) {
-        val container = root.center as ScrollPane
-        val atBottom = container.vvalue > 0.999
+        runLater {
+            //val container = root.center as ScrollPane
+            //val atBottom = container.vvalue > 0.999
 
-        output.children.add(text)
+            val engine = output.engine
 
-        if (atBottom) {
-            output.layout()
-            container.layout()
-            container.vvalue = 1.0
+            if (engine.loadWorker.state == Worker.State.SUCCEEDED) {
+                val doc = engine.document
+                val body = doc.getElementsByTagName("body").item(0)
+                val elementNode = doc.createElement("div")
+                val textNode = doc.createTextNode(text.text)
+                elementNode.appendChild(textNode)
+                body.appendChild(elementNode)
 
+            }
+
+            /*if (atBottom) {
+                output.layout()
+                container.layout()
+                container.vvalue = 1.0
+
+            }*/
         }
     }
 }
