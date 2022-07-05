0edd0738454e9e387801aa0fe0de367bbaa3276c
Sean Proctor <sproctor@gmail.com>
2022-07-05 13:30:21 -0400

    release 1.0.0-beta01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "warlock3"
-            packageVersion = "1.0.10"
+            packageVersion = "1.0.11"
 
             windows {
                 menu = true
