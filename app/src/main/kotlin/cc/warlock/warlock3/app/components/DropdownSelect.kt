922b527b0692c20484e6ad2cf69f5a5ec4dcb66a
Sean Proctor <sproctor@gmail.com>
2022-10-31 09:58:30 -0400

    Fix issues with dropdown selector

--- app/src/main/kotlin/cc/warlock/warlock3/app/components/DropdownSelect.kt
+++ app/src/main/kotlin/cc/warlock/warlock3/app/components/DropdownSelect.kt
@@ -1,6 +1,5 @@
 package cc.warlock.warlock3.app.components
 
-import androidx.compose.foundation.gestures.forEachGesture
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -8,14 +7,6 @@ import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.PointerEventPass
-import androidx.compose.ui.input.pointer.changedToUp
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.onClick
-import androidx.compose.ui.semantics.semantics
-import kotlinx.coroutines.coroutineScope
 
 @Composable
 fun <T> DropdownSelect(
@@ -27,14 +18,7 @@ fun <T> DropdownSelect(
     onSelect: (T) -> Unit,
 ) {
     var expanded by remember { mutableStateOf(false) }
-    Box(
-        modifier = modifier.expandable(
-            onExpandedChange = {
-                expanded = !expanded
-            },
-            menuLabel = "dropdown menu"
-        )
-    ) {
+    Box(modifier) {
         OutlinedTextField(
             value = itemLabelBuilder(selected),
             label = label,
@@ -42,15 +26,14 @@ fun <T> DropdownSelect(
             readOnly = true,
             trailingIcon = {
                 IconButton(
-                    onClick = {
-                        expanded = !expanded
-                    },
+                    onClick = { expanded = true },
+                    enabled = !expanded,
                 ) {
                     Icon(
                         imageVector = Icons.Default.ArrowDropDown,
                         contentDescription = "select",
                         Modifier.rotate(
-                            if (expanded) 360f else 180f
+                            if (expanded) 180f else 0f
                         ),
                     )
                 }
@@ -71,30 +54,5 @@ fun <T> DropdownSelect(
                 }
             }
         }
-    }
-}
-
-private fun Modifier.expandable(
-    onExpandedChange: () -> Unit,
-    menuLabel: String
-) = pointerInput(Unit) {
-    forEachGesture {
-        coroutineScope {
-            awaitPointerEventScope {
-                var event: PointerEvent
-                do {
-                    event = awaitPointerEvent(PointerEventPass.Initial)
-                } while (
-                    !event.changes.all { it.changedToUp() }
-                )
-                onExpandedChange.invoke()
-            }
-        }
-    }
-}.semantics {
-    contentDescription = menuLabel // this should be a localised string
-    onClick {
-        onExpandedChange()
-        true
     }
 }
