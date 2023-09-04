704ccb1d44654f2782dacf3efb56296bee5694b6
Sean Proctor <sproctor@gmail.com>
2023-09-04 19:33:37 -0400

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,5 +24,5 @@ dependencyResolutionManagement {
 
 plugins {
     // See https://jmfayard.github.io/refreshVersions
-    id("de.fayard.refreshVersions") version "0.60.1"
+    id("de.fayard.refreshVersions") version "0.60.2"
 }
