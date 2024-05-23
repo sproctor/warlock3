3cdaf689a00051ab26c865fac9a77126d92ad2e6
Sean Proctor <sproctor@gmail.com>
2024-05-23 18:15:56 -0400

    only look at updates with the same or better stability

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,8 +31,12 @@ plugins {
     id("de.fayard.refreshVersions") version "0.60.5"
 }
 
-// work-around https://github.com/Splitties/refreshVersions/issues/640
 refreshVersions {
+    // work-around https://github.com/Splitties/refreshVersions/issues/640
     file("build/tmp/refreshVersions").mkdirs()
     versionsPropertiesFile = file("build/tmp/refreshVersions/versions.properties")
+
+    rejectVersionIf {
+        candidate.stabilityLevel.isLessStableThan(current.stabilityLevel)
+    }
 }
