5b41eff0868558de430992df829a25876d1b49c8
Sean Proctor <sproctor@gmail.com>
2024-10-15 17:27:38 -0400

    update deprecated `.menuAnchor` call

--- compose/src/commonMain/kotlin/warlockfe/warlock3/compose/components/DropdownSelect.kt
+++ compose/src/commonMain/kotlin/warlockfe/warlock3/compose/components/DropdownSelect.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,7 +33,7 @@ fun <T> DropdownSelect(
         }
     ) {
         OutlinedTextField(
-            modifier = Modifier.menuAnchor(),
+            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable),
             readOnly = true,
             value = itemLabelBuilder(selected),
             onValueChange = { },
