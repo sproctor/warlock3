c194b2a8f333df9a7d04bff55141d62b706481fc
Sean Proctor <sproctor@gmail.com>
2024-12-04 18:28:11 -0500

    fix android compile issue

--- android/src/main/kotlin/warlockfe/warlock3/android/WarlockApp.kt
+++ android/src/main/kotlin/warlockfe/warlock3/android/WarlockApp.kt
@@ -112,7 +112,6 @@ fun WarlockApp(
                             scriptDirRepository = appContainer.scriptDirRepository,
                             alterationRepository = appContainer.alterationRepository,
                             characterRepository = appContainer.characterRepository,
-                            clientSettingRepository = appContainer.clientSettings,
                         )
                     }
                 }
