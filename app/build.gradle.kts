6f91291d8108e95d0be63f640b20f790f5118b8f
Sean Proctor <sproctor@gmail.com>
2024-12-22 09:00:20 -0500

    switch back to Java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,7 @@ compose.desktop {
         }
 
         buildTypes.release.proguard {
-            // version.set("7.6.0")
+            version.set("7.6.0")
             configurationFiles.from("rules.pro")
         }
     }
