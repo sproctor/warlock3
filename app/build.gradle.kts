d2799dc54390adba22e0deca8b4b585bd77c022e
Sean Proctor <sproctor@gmail.com>
2025-04-17 22:08:21 -0400

    update kotlin logging and add workaround for proguard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     // Logging and error reporting
     implementation(libs.slf4j.simple)
     implementation(libs.sentry.kotlin)
+    implementation(libs.logback.classic)
 
     // Control updates
     implementation(libs.conveyor.control)
