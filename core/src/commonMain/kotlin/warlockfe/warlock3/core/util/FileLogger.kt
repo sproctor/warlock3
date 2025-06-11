707e0946ea7033e4d8a393aba317116f69a26041
Sean Proctor <sproctor@gmail.com>
2025-06-11 18:04:43 -0400

    generate filename using time as well as date

--- core/src/commonMain/kotlin/warlockfe/warlock3/core/util/FileLogger.kt
+++ core/src/commonMain/kotlin/warlockfe/warlock3/core/util/FileLogger.kt
@@ -9,43 +9,38 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class FileLogger(
-    private val directory: Path,
+    directory: Path,
+    timestamp: LocalDateTime = LocalDateTime.now()
 ): AutoCloseable {
 
     private val context = Dispatchers.IO.limitedParallelism(1)
 
-    private var cachedStream: BufferedWriter? = null
-    private var dateString: String? = null
+    private val outputStream: FileOutputStream
+    private val writer: BufferedWriter
+
+    init {
+        val formattedDate = timestamp.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"))
+        val path = directory / "$formattedDate.log"
+        path.parent?.toFile()?.mkdirs()
+        outputStream = FileOutputStream(path.toFile(), true)
+        writer = outputStream.bufferedWriter()
+    }
 
     suspend fun write(message: String, addTimestamps: Boolean) {
         withContext(context) {
-            val now = LocalDateTime.now()
-            val stream = getStream(now)
             if (addTimestamps) {
+                val now = LocalDateTime.now()
                 val dateString = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"))
-                stream.write("[$dateString] $message\n")
+                writer.write("[$dateString] $message\n")
             } else {
-                stream.write("$message\n")
+                writer.write("$message\n")
             }
-            stream.flush()
+            writer.flush()
         }
-    }
-
-    private fun getStream(now: LocalDateTime): BufferedWriter {
-        val formattedDate = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
-        if (cachedStream == null || dateString != formattedDate) {
-            val file = directory / "$formattedDate.log"
-            file.parent?.toFile()?.mkdirs()
-            cachedStream?.close()
-            cachedStream = FileOutputStream(file.toFile(), true).bufferedWriter()
-            dateString = formattedDate
-        }
-        return cachedStream!!
     }
 
     override fun close() {
-        cachedStream?.close()
-        cachedStream = null
-        dateString = null
+        writer.close()
+        outputStream.close()
     }
 }
