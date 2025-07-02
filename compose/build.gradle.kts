6e069114d5fd50f412067ffb5b9e675e236c7c14
Sean Proctor <sproctor@gmail.com>
2025-07-02 10:19:03 -0400

    move constraint layout into gradle catalog

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -25,7 +25,7 @@ kotlin {
                 implementation(compose.components.resources)
                 implementation(libs.material.icons)
                 api(libs.androidx.lifecycle.viewmodel.compose)
-                implementation("tech.annexflow.compose:constraintlayout-compose-multiplatform:0.6.0")
+                implementation(libs.constraintlayout)
                 implementation(compose.uiTooling)
                 implementation(compose.components.uiToolingPreview)
 
