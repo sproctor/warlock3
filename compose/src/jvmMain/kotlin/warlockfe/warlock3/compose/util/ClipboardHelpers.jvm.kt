39b485b43533eece3123a8a8e874ea35e27027a3
Sean Proctor <sproctor@gmail.com>
2025-10-16 07:30:43 -0400

    fix crash while pasting

--- compose/src/jvmMain/kotlin/warlockfe/warlock3/compose/util/ClipboardHelpers.jvm.kt
+++ compose/src/jvmMain/kotlin/warlockfe/warlock3/compose/util/ClipboardHelpers.jvm.kt
@@ -3,14 +3,20 @@ package warlockfe.warlock3.compose.util
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.platform.ClipEntry
 import androidx.compose.ui.platform.asAwtTransferable
+import io.github.oshai.kotlinlogging.KLogger
+import kotlinx.io.IOException
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.StringSelection
 
 @OptIn(ExperimentalComposeUiApi::class)
 actual fun ClipEntry.getText(): String? {
     val transferable = asAwtTransferable ?: return null
-    if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
-        return transferable.getTransferData(DataFlavor.stringFlavor) as String
+    try {
+        if (transferable.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+            return transferable.getTransferData(DataFlavor.stringFlavor) as String
+        }
+    } catch (_: IOException) {
+        // ignore issues retrieving the clipboard contents.
     }
     return null
 }
