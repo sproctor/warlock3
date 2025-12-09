ab13f174ef91ecb14fd637f150c4b2ad3cfa2465
Sean Proctor <sproctor@gmail.com>
2025-12-09 15:53:36 -0500

    fix icon color

--- app/src/main/kotlin/warlockfe/warlock3/app/TitleBarView.kt
+++ app/src/main/kotlin/warlockfe/warlock3/app/TitleBarView.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -66,6 +67,7 @@ internal fun DecoratedWindowScope.TitleBarView(
                                 Res.drawable.space_dashboard_outlined
                             }
                         ),
+                        tint = MaterialTheme.colorScheme.onSurface,
                         contentDescription = null,
                     )
                 }
