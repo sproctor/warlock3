7e36885f4e5fb14555bedb2463a5182027114e90
Sean Proctor <sproctor@gmail.com>
2025-12-12 07:53:59 -0500

    get ready to actually release 131

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,7 +77,7 @@ compose {
                 targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
 
                 // args("--input=/home/sproctor/Downloads/20251116072204.log")
-                args("--input=/home/sproctor/.local/state/warlock/logs/DR_Tefrin/20251122120309.log")
+                // args("--input=/home/sproctor/.local/state/warlock/logs/DR_Tefrin/20251122120309.log")
                 // args("--sge-port=7900", "--sge-secure=off")
                 // args("--connection=Tefrin")
 
