bcfe9b2e2bff933ff9e38815aa8a676d714ac5bb
Sean Proctor <sproctor@gmail.com>
2025-12-11 23:02:03 -0500

    use a text button in the about dialog

--- app/src/main/kotlin/warlockfe/warlock3/app/AboutDialog.kt
+++ app/src/main/kotlin/warlockfe/warlock3/app/AboutDialog.kt
@@ -10,9 +10,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,7 +48,7 @@ fun AboutDialog(
                     }
                 }
                 Box(Modifier.fillMaxSize()) {
-                    Button(
+                    TextButton(
                         modifier = Modifier.align(Alignment.BottomEnd),
                         onClick = onCloseRequest
                     ) {
