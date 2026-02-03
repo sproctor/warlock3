7d0b28fa92ef6a1175ac680934e32bb09913dbde
Sean Proctor <sproctor@gmail.com>
2026-02-03 17:38:34 -0500

    update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-plugins{
+plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.android.application) apply false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,7 +23,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://www.jetbrains.com/intellij-repository/releases")
-        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+        //maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
         mavenLocal()
     }
 }
