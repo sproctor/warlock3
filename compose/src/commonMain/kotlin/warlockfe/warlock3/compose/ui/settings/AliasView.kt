0f2bd01b0eba532488d115ad9dfb79e7d9c0734b
Sean Proctor <sproctor@gmail.com>
2026-03-17 15:49:33 -0400

    improve appearance of alias

--- compose/src/commonMain/kotlin/warlockfe/warlock3/compose/ui/settings/AliasView.kt
+++ compose/src/commonMain/kotlin/warlockfe/warlock3/compose/ui/settings/AliasView.kt
@@ -7,11 +7,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.input.TextFieldLineLimits
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
@@ -25,12 +25,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.painterResource
@@ -43,7 +42,6 @@ import warlockfe.warlock3.core.prefs.models.AliasEntity
 import warlockfe.warlock3.core.prefs.repositories.AliasRepository
 import kotlin.uuid.Uuid
 
-@OptIn(DelicateCoroutinesApi::class)
 @Composable
 fun AliasView(
     currentCharacter: GameCharacter?,
@@ -52,8 +50,10 @@ fun AliasView(
 ) {
     var selectedCharacter by remember(currentCharacter) { mutableStateOf(currentCharacter) }
     val currentCharacterId = selectedCharacter?.id ?: "global"
-    val aliases by aliasRepository.observeByCharacter(currentCharacterId).collectAsState(emptyList())
+    val aliases by aliasRepository.observeByCharacter(currentCharacterId)
+        .collectAsState(emptyList())
     var editingAlias by remember { mutableStateOf<AliasEntity?>(null) }
+    val coroutineScope = rememberCoroutineScope()
 
     Column(Modifier.fillMaxSize()) {
         SettingsCharacterSelector(
@@ -76,15 +76,21 @@ fun AliasView(
                             IconButton(
                                 onClick = { editingAlias = alias }
                             ) {
-                                Icon(painter = painterResource(Res.drawable.edit), contentDescription = "Edit")
+                                Icon(
+                                    painter = painterResource(Res.drawable.edit),
+                                    contentDescription = "Edit"
+                                )
                             }
                             Spacer(Modifier.width(8.dp))
                             IconButton(
                                 onClick = {
-                                    GlobalScope.launch { aliasRepository.deleteById(alias.id) }
+                                    coroutineScope.launch { aliasRepository.deleteById(alias.id) }
                                 }
                             ) {
-                                Icon(painter = painterResource(Res.drawable.delete), contentDescription = "Delete")
+                                Icon(
+                                    painter = painterResource(Res.drawable.delete),
+                                    contentDescription = "Delete"
+                                )
                             }
                         }
                     }
@@ -95,23 +101,30 @@ fun AliasView(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.End
         ) {
-            IconButton(onClick = {
-                editingAlias = AliasEntity(
-                    id = Uuid.random(),
-                    characterId = currentCharacterId,
-                    pattern = "",
-                    replacement = "",
-                )
-            }) {
-                Icon(painter = painterResource(Res.drawable.add), contentDescription = null)
-            }
+            ExtendedFloatingActionButton(
+                onClick = {
+                    editingAlias = AliasEntity(
+                        id = Uuid.random(),
+                        characterId = currentCharacterId,
+                        pattern = "",
+                        replacement = "",
+                    )
+                },
+                icon = {
+                    Icon(
+                        painter = painterResource(Res.drawable.add),
+                        contentDescription = null
+                    )
+                },
+                text = { Text("New alias") }
+            )
         }
     }
     editingAlias?.let { alias ->
         EditAliasDialog(
             alias = alias,
             saveAlias = { newAlias ->
-                GlobalScope.launch {
+                coroutineScope.launch {
                     aliasRepository.save(newAlias)
                     editingAlias = null
                 }
@@ -129,6 +142,7 @@ fun EditAliasDialog(
 ) {
     val pattern = rememberTextFieldState(alias.pattern)
     val replacement = rememberTextFieldState(alias.replacement)
+    var patternError by remember { mutableStateOf<String?>(null) }
 
     AlertDialog(
         onDismissRequest = onClose,
@@ -143,7 +157,8 @@ fun EditAliasDialog(
                             replacement = replacement.text.toString(),
                         )
                     )
-                }
+                },
+                enabled = patternError == null,
             ) {
                 Text("Save")
             }
@@ -153,13 +168,12 @@ fun EditAliasDialog(
                 Text("Cancel")
             }
         },
+        title = { Text("Edit Alias") },
         text = {
             Column(
-                modifier = Modifier.padding(24.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                var patternError by remember { mutableStateOf<String?>(null) }
-                LaunchedEffect(pattern) {
+                LaunchedEffect(Unit) {
                     snapshotFlow { pattern.text.toString() }
                         .collectLatest {
                             try {
