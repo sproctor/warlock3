fb34032bfe16b0bf8ca636b3e53c19f541f975f9
Sean Proctor <sproctor@gmail.com>
2026-03-17 19:06:58 -0400

    switch to windows style argument processing

--- core/src/jvmMain/kotlin/warlockfe/warlock3/core/client/JavaProxy.kt
+++ core/src/jvmMain/kotlin/warlockfe/warlock3/core/client/JavaProxy.kt
@@ -2,7 +2,6 @@ package warlockfe.warlock3.core.client
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.asFlow
-import java.net.Socket
 
 class JavaProxy(command: String) : WarlockProxy {
     private val process = ProcessBuilder(splitCommand(command)).start()
@@ -23,30 +22,46 @@ private fun splitCommand(command: String): List<String> {
     val args = mutableListOf<String>()
     val current = StringBuilder()
     var inQuotes = false
-    var quoteChar = ' '
-    var escaped = false
+    var i = 0
 
-    for (ch in command) {
-        when {
-            escaped -> {
-                current.append(ch)
-                escaped = false
+    while (i < command.length) {
+        val ch = command[i]
+        // Count run of backslashes
+        if (ch == '\\') {
+            var numBackslashes = 0
+            while (i < command.length && command[i] == '\\') {
+                numBackslashes++
+                i++
             }
-            ch == '\\' -> escaped = true
-            inQuotes && ch == quoteChar -> inQuotes = false
-            !inQuotes && (ch == '"' || ch == '\'') -> {
-                inQuotes = true
-                quoteChar = ch
-            }
-            !inQuotes && ch == ' ' -> {
-                if (current.isNotEmpty()) {
-                    args.add(current.toString())
-                    current.clear()
+            if (i < command.length && command[i] == '"') {
+                // Backslashes before a quote: 2n -> n literal backslashes + quote toggles
+                // 2n+1 -> n literal backslashes + literal quote
+                current.append("\\".repeat(numBackslashes / 2))
+                if (numBackslashes % 2 == 1) {
+                    current.append('"')
+                } else {
+                    inQuotes = !inQuotes
                 }
+                i++ // consume the quote
+            } else {
+                // Backslashes not before a quote: all literal
+                current.append("\\".repeat(numBackslashes))
             }
-            else -> current.append(ch)
+        } else if (ch == '"') {
+            inQuotes = !inQuotes
+            i++
+        } else if (!inQuotes && (ch == ' ' || ch == '\t')) {
+            if (current.isNotEmpty()) {
+                args.add(current.toString())
+                current.clear()
+            }
+            i++
+        } else {
+            current.append(ch)
+            i++
         }
     }
+
     if (current.isNotEmpty()) args.add(current.toString())
     return args
 }
