d7414600a0b13a36a28d2528d07e7692c9ee6519
Sean Proctor <sproctor@gmail.com>
2026-03-17 04:18:01 -0400

    fix longstanding clipboard crash

--- compose/src/commonMain/kotlin/warlockfe/warlock3/compose/util/ClipboardHelpers.kt
+++
@@ -1,8 +0,0 @@
-package warlockfe.warlock3.compose.util
-
-import androidx.compose.ui.platform.ClipEntry
-import androidx.compose.ui.text.AnnotatedString
-
-expect fun ClipEntry.getText(): String?
-
-expect fun createClipEntry(text: String): ClipEntry
