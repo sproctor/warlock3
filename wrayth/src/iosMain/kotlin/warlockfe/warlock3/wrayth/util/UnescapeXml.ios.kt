ebb70e8fd6632862682a1fa1cb0e4f214f40f42e
Sean Proctor <sproctor@gmail.com>
2026-04-22 20:22:49 -0400

    implement custom unescapeXml logic for iOS

--- wrayth/src/iosMain/kotlin/warlockfe/warlock3/wrayth/util/UnescapeXml.ios.kt
+++ wrayth/src/iosMain/kotlin/warlockfe/warlock3/wrayth/util/UnescapeXml.ios.kt
@@ -1,21 +1,55 @@
 package warlockfe.warlock3.wrayth.util
 
-import kotlinx.cinterop.BetaInteropApi
-import kotlinx.cinterop.ExperimentalForeignApi
-import platform.Foundation.NSAttributedString
-import platform.Foundation.NSString
-import platform.Foundation.NSUTF8StringEncoding
-import platform.Foundation.create
-import platform.Foundation.dataUsingEncoding
-import platform.UIKit.NSDocumentTypeDocumentAttribute
-import platform.UIKit.NSHTMLTextDocumentType
-import platform.UIKit.create
-
-@OptIn(BetaInteropApi::class, ExperimentalForeignApi::class)
 actual fun unescapeXml(text: String): String {
-    val nsString = NSString.create(text)
-    val data = nsString.dataUsingEncoding(NSUTF8StringEncoding)
-    val options = mapOf<Any?, Any>(NSDocumentTypeDocumentAttribute to NSHTMLTextDocumentType!!)
-    val attributedString = NSAttributedString.create(data = data!!, options = options, documentAttributes = null, error = null)
-    return attributedString!!.string()
+    if ('&' !in text) return text
+    val result = StringBuilder(text.length)
+    var i = 0
+    while (i < text.length) {
+        val c = text[i]
+        if (c != '&') {
+            result.append(c)
+            i++
+            continue
+        }
+        val semi = text.indexOf(';', startIndex = i + 1)
+        if (semi < 0) {
+            result.append(c)
+            i++
+            continue
+        }
+        val entity = text.substring(i + 1, semi)
+        val consumed = when {
+            entity == "amp" -> result.append('&').let { true }
+            entity == "lt" -> result.append('<').let { true }
+            entity == "gt" -> result.append('>').let { true }
+            entity == "quot" -> result.append('"').let { true }
+            entity == "apos" -> result.append('\'').let { true }
+            entity.startsWith("#x") || entity.startsWith("#X") ->
+                entity.substring(2).toIntOrNull(16)?.let { result.appendCodePoint(it); true } == true
+
+            entity.startsWith("#") ->
+                entity.substring(1).toIntOrNull()?.let { result.appendCodePoint(it); true } == true
+
+            else -> false
+        }
+        if (consumed) {
+            i = semi + 1
+        } else {
+            result.append(c)
+            i++
+        }
+    }
+    return result.toString()
+}
+
+private fun StringBuilder.appendCodePoint(codePoint: Int) {
+    when {
+        codePoint !in 0..0x10FFFF -> Unit
+        codePoint <= 0xFFFF -> append(codePoint.toChar())
+        else -> {
+            val adjusted = codePoint - 0x10000
+            append(((adjusted ushr 10) + 0xD800).toChar())
+            append(((adjusted and 0x3FF) + 0xDC00).toChar())
+        }
+    }
 }
